7b06fa7d7c15b862a29bc51dc989013d97d5b0e7
Ronnie Gross <rnngrss1495@gmail.co>
2019-02-10 19:16:36 -0500

    linked jquery and velocity.js

---
+++ velocityTalk.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+});
+
+// console.log( $("li") );
+
+// $(document).ready(function() {
+//     document.write("Hello, World!");
+//  });
