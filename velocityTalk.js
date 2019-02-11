4d535145a54771cff12246fb8e2f3bef76b9b0d2
Ronnie Gross <rnngrss1495@gmail.co>
2019-02-10 20:34:44 -0500

    still foolin with velocity.js

--- velocityTalk.js
+++ velocityTalk.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-    console.log( "ready!" );
-});
+// $("h1").velocity("transition.slideLeftIn", { stagger: 1000, drag: true });
 
-// console.log( $("li") );
+// $("h1").velocity("fadeIn", { duration: 1500 });
+// $("h2").velocity("slideOut", {opacity: 0.0}, { delay: 4000 }, { duration: 1000 });
+// $(".blueBar").velocity("fadeIn", {opacity: 0}, {delay: 4000 }, { duration: 1000 });
 
-// $(document).ready(function() {
-//     document.write("Hello, World!");
-//  });
+
+$("h1").velocity( { opacity: .7}, 1000, "swing", function() { console.log("effect done."); } );
+
+// $("h1")
+//   .velocity( { opacity: 0.5 }, { duration: 1000 } )
+//   .velocity( { opacity: 1.0 }, { duration: 1000 } );
+
+//   $("h1").velocity( { opacity: 0.1 }, { duration: 500, loop: 2 } );
+
+// $("h1").velocity("transition.fadeIn");
