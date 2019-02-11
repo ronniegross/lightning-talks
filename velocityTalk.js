a701fcd857bd4f4b917873fae3e466979b3501c8
Ronnie Gross <rnngrss1495@gmail.co>
2019-02-10 21:26:47 -0500

    created arrow transitions with velocity.js

--- velocityTalk.js
+++ velocityTalk.js
@@ -5,7 +5,7 @@
 // $(".blueBar").velocity("fadeIn", {opacity: 0}, {delay: 4000 }, { duration: 1000 });
 
 
-$("h1").velocity( { opacity: .7}, 1000, "swing", function() { console.log("effect done."); } );
+// $("h1").velocity( { opacity: .7}, 1000, "swing", function() { console.log("effect done."); } );
 
 // $("h1")
 //   .velocity( { opacity: 0.5 }, { duration: 1000 } )
@@ -14,3 +14,51 @@ $("h1").velocity( { opacity: .7}, 1000, "swing", function() { console.log("effec
 //   $("h1").velocity( { opacity: 0.1 }, { duration: 500, loop: 2 } );
 
 // $("h1").velocity("transition.fadeIn");
+
+$(".sectionOne").velocity("fadeIn", function(){
+	$(".arrow").velocity(
+        // {translateX: "90%"},
+		{ translateY: "10px" },
+		{ duration: 800, loop: true }
+	);
+});
+
+$("#arrow1").click(function(){
+	$(".sectionTwo").velocity({ opacity: 0 }, function(){
+		$(this)
+			.velocity("scroll", 500)
+			.velocity({ opacity: 1 });
+	});
+});
+
+$("#arrow2").click(function(){
+	$(".sectionThree").velocity({ opacity: 0 }, function(){
+		$(this)
+			.velocity("scroll", 500)
+			.velocity({ opacity: 1 });
+	});
+});
+
+$("#arrow3").click(function(){
+	$(".sectionFour").velocity({ opacity: 0 }, function(){
+		$(this)
+			.velocity("scroll", 500)
+			.velocity({ opacity: 1 });
+	});
+});
+
+$("#arrow4").click(function(){
+	$(".sectionFive").velocity({ opacity: 0 }, function(){
+		$(this)
+			.velocity("scroll", 500)
+			.velocity({ opacity: 1 });
+	});
+});
+
+$("#arrow5").click(function(){
+	$(".sectionSix").velocity({ opacity: 0 }, function(){
+		$(this)
+			.velocity("scroll", 500)
+			.velocity({ opacity: 1 });
+	});
+});
